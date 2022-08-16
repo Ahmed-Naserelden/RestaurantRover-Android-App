46912ccb75ae75ad6c63f16fff6191252125ab28
Ahmed Nser El-Din <72213733+Ahmed-Naserelden@users.noreply.github.com>
2022-08-16 23:24:23 +0200

    some edit

--- app/src/main/java/com/example/restaurantmobileapp/MainActivity.java
+++ app/src/main/java/com/example/restaurantmobileapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.restaurantmobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -14,16 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Product p = new Product();
-
+    }
+    public void save(View view){
         FirebaseDatabase fir = FirebaseDatabase.getInstance();
-        DatabaseReference ref = fir.getReference();
+        DatabaseReference ref = fir.getReference("Restaurant");
+        Product p = new Product();
+        p.setName("rich");
+        p.setDetail("slat");
+        p.setType("food");
+        p.setPrice(15.3f);
 
-        ref.push().setValue(p);
+        ref.child("Users").push().setValue(p).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
 
+                    }
+                }).addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Toast.makeText(MainActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
 
-
-
-
+                    }
+                });
     }
 }
